808692ddce5fb96fb81b2cb5e902ca775803eab8
Sanhak <104179839+hacked34354tf@users.noreply.github.com>
2022-07-03 14:03:32 +0300

    Update Listeners.java

--- src/me/sanhak/autoroles/listeners/Listeners.java
+++ src/me/sanhak/autoroles/listeners/Listeners.java
@@ -6,13 +6,13 @@ import net.dv8tion.jda.api.hooks.*;
 
 public class Listeners extends ListenerAdapter { //Start
 	
-	@Override
+    @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {	
-		//Roles 
-		Role member = e.getJDA().getRoleById("989582280383148074"); 
-		Role bot = e.getJDA().getRoleById("Role-ID");
+	//Roles 
+	Role member = e.getJDA().getRoleById("989582280383148074"); 
+	Role bot = e.getJDA().getRoleById("Role-ID");
 		
-		//Check if the user is a bot or a player
+	//Check if the user is a bot or a player
         if (e.getMember().getUser().isBot()) { // Checking if the user is a bot will be given the role of bot
         	e.getGuild().addRoleToMember(e.getMember(), bot).queue();      
         } else { // If the member is not a bot, the role of the member will be given
