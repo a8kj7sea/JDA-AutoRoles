1a773cd316530cc39c6bdc0e3a34d649f4abd277
Sanhak <104179839+hacked34354tf@users.noreply.github.com>
2022-07-13 17:01:41 +0300

    Update Listeners.java

--- src/me/sanhak/autoroles/listeners/Listeners.java
+++ src/me/sanhak/autoroles/listeners/Listeners.java
@@ -14,9 +14,9 @@ public class Listeners extends ListenerAdapter { //Start
 		
 	//Check if the user is a bot or a player
         if (e.getMember().getUser().isBot()) { // Checking if the user is a bot will be given the role of bot
-        	e.getGuild().addRoleToMember(e.getMember(), bot).queue();      
+            e.getGuild().addRoleToMember(e.getMember(), bot).queue();      
         } else { // If the member is not a bot, the role of the member will be given
-        	e.getGuild().addRoleToMember(e.getMember(), member).queue();       
+            e.getGuild().addRoleToMember(e.getMember(), member).queue();       
         }
     }
    
